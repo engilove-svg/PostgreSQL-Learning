f96b8e4a6eaf4a081017af9ba38728bfd6572170
loveleen kaur <engi.asees@gmail.com>
2026-09-24 20:57:09 -0400

    Day 30 CASE

---
+++ day30.sql
@@ -0,0 +1,18 @@
+SELECT name,
+       salary,
+       CASE
+           WHEN salary >= 70000 THEN 'High'
+           WHEN salary >= 50000 THEN 'Medium'
+           ELSE 'Low'
+       END AS salary_level
+FROM employees;
+
+SELECT position,
+       status,
+       CASE
+           WHEN status = 'Offer' THEN 'Successful'
+           WHEN status = 'Interview' THEN 'In Progress'
+           WHEN status = 'Applied' THEN 'In Progress'
+           ELSE 'Closed'
+       END AS application_stage
+FROM applications;
