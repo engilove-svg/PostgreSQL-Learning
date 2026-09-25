a51e679001226ff9732b69189278ff3624e74a97
loveleen kaur <engi.asees@gmail.com>
2026-09-24 20:51:36 -0400

    Day 29 joins clauses

--- day 29.sql
+++ day 29.sql
@@ -7,3 +7,33 @@ SELECT employees.name, departments.department_name
 FROM employees
 LEFT JOIN departments
 ON employees.department_id = departments.id;
+
+SELECT employees.name, departments.department_name
+FROM employees
+INNER JOIN departments
+ON employees.department_id = departments.id
+WHERE departments.department_name = 'IT';
+
+SELECT employees.name,
+       departments.department_name,
+       employees.salary
+FROM employees
+INNER JOIN departments
+ON employees.department_id = departments.id
+ORDER BY employees.salary DESC;
+
+SELECT departments.department_name,
+       COUNT(employees.id)
+FROM departments
+LEFT JOIN employees
+ON departments.id = employees.department_id
+GROUP BY departments.department_name;
+
+SELECT applications.position,
+       companies.company_name,
+       industries.industry_name
+FROM applications
+INNER JOIN companies
+ON applications.company_id = companies.id
+INNER JOIN industries
+ON companies.industry_id = industries.id;
